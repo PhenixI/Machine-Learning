83e74308b5e67c17f4f84167d4ba4b3d4aba220e
PhenixI <zhanzhanlove.hi@gmail.com>
2015-10-09 22:26:52 +0800

    forward propagation and Calculate Cost
    A:forward propagation step: 1.Convolution, 2.pooling;3.softmax (dense
    connected)
    B: Calculate Cost

--- 7-Convolution Neural Networks/cnn/cnnTrain.m
+++ 7-Convolution Neural Networks/cnn/cnnTrain.m
@@ -20,10 +20,10 @@ numFilters = 20;   % Number of filters for conv layer
 poolDim = 2;      % Pooling dimension, (should divide imageDim-filterDim+1)
 
 % Load MNIST Train
-addpath ../common/;
-images = loadMNISTImages('../common/train-images-idx3-ubyte');
+addpath ../../common/;
+images = loadMNISTImages('../../data/common/train-images-idx3-ubyte');
 images = reshape(images,imageDim,imageDim,[]);
-labels = loadMNISTLabels('../common/train-labels-idx1-ubyte');
+labels = loadMNISTLabels('../../data/common/train-labels-idx1-ubyte');
 labels(labels==0) = 10; % Remap 0 to 10
 
 % Initialize Parameters
@@ -90,9 +90,9 @@ opttheta = minFuncSGD(@(x,y,z) cnnCost(x,y,z,numClasses,filterDim,...
 %  Test the performance of the trained model using the MNIST test set. Your
 %  accuracy should be above 97% after 3 epochs of training
 
-testImages = loadMNISTImages('../common/t10k-images-idx3-ubyte');
+testImages = loadMNISTImages('../../data/common/t10k-images-idx3-ubyte');
 testImages = reshape(testImages,imageDim,imageDim,[]);
-testLabels = loadMNISTLabels('../common/t10k-labels-idx1-ubyte');
+testLabels = loadMNISTLabels('../../data/common/t10k-labels-idx1-ubyte');
 testLabels(testLabels==0) = 10; % Remap 0 to 10
 
 [~,cost,preds]=cnnCost(opttheta,testImages,testLabels,numClasses,...
