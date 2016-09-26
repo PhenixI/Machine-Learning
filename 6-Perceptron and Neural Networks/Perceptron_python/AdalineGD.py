7b4cc1d7233dcf3d2ba6e4f12a03e29585b1e216
PhenixI <zhanzhanlove.hi@gmail.com>
2016-09-26 18:01:18 +0800

    Adaline algorithm
    Adaptive Linear Neuron algorithm (ADALine) ,optimizted using Gradient
    Descend

--- 6-Perceptron and Neural Networks/Perceptron_python/AdalineGD.py
+++ 6-Perceptron and Neural Networks/Perceptron_python/AdalineGD.py
@@ -1,6 +1,6 @@
 import numpy as np
-class Perceptron(object):
-    """Perceptron classifier.
+class AdalineGD(object):
+    """AdAptive LInear NEuron classifier.
     Parameters
     ------------
     eta : float
@@ -14,11 +14,11 @@ class Perceptron(object):
     errors_ : list
     Number of misclassifications in every epoch.
     """
-    def __init__(self,eta = 0.01, n_iter = 10):
+    def __init__(self,eta = 0.01, n_iter = 50):
         self.eta = eta
         self.n_iter = n_iter
 
-    def fit(self,X,y)：
+    def fit(self,X,y):
         """Fit training data.
         Parameters
         ----------
@@ -33,24 +33,26 @@ class Perceptron(object):
         self : object
         """
         self.w_ = np.zeros(1+X.shape[1])
-        self.errors_ = []
+        self.cost_ = []
 
         for _ in range(self.n_iter):
-            errors = 0
-            for xi,target in zip(X,y):
-                update = self.eta * (target-self.predict(xi))
-                self.w_[1:] += update * xi
-                self.w_[0]  += update
-                errors += int(update != 0.0)
-            self.errors_.append(errors)
+            output = self.net_input(X)
+            errors = (y-output)
+            self.w_[1:] += self.eta * X.T.dot(errors)
+            self.w_[0]  += self.eta * errors.sum()
+            cost = (errors**2).sum()/2.0
+            self.cost_.append(cost)
         return self
     
     def net_input(self,X):
         '''calculate net input'''
         return np.dot(X,self.w_[1:])+self.w_[0]
+    def activation(self,X):
+        '''compute linear activation'''
+        return self.net_input(X)
 
     def predict(self,X):
         '''Return class label after unit step'''
-        return np.where(self.net_input(X) >= 0.0,1,-1)
+        return np.where(self.activation(X) >= 0.0,1,-1)
                 
         
