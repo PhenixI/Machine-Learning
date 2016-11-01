3c00b433ed86d0c00d9871720ab7de7fd2551edf
PhenixI <zhanzhanlove.hi@gmail.com>
2016-11-01 16:33:07 +0800

    Data preprocessing
    Data preprocessing:
    1.How to handle missing data;
    2.Handle categorical data;
    3.Partitioning a dataset in training and testsets
    4.Bring features onto same scale
    5.selecting meaingful features

--- 0-Data Preprocessing/data_preprocessing/data_preprocessing.py
+++ 0-Data Preprocessing/data_preprocessing/data_preprocessing.py
@@ -37,7 +37,7 @@ size_mapping = {'XL':3,'L':2,'M':1}
 df['size'] = df['size'].map(size_mapping)
 df
 
-#Encoding class labels
+#2.Encoding class labels
 import numpy as np
 class_mapping = {label:idx for idx,label in enumerate(np.unique(df['classlabel']))}
 class_mapping
@@ -54,7 +54,7 @@ class_le = LabelEncoder()
 y = class_le.fit_transform(df['classlabel'].values)
 y
 
-#performing one-hot encoding on nominal features
+#3.performing one-hot encoding on nominal features
 
 #a.
 X = df[['color', 'size', 'price']].values
@@ -69,6 +69,88 @@ ohe.fit_transform(X).toarray()
 pd.get_dummies(df[['price', 'color', 'size']])
 
 
+#Partitioning a dataset in training and testsets
+
+#dataset,Wine,https://archive.ics.uci.edu/ml/datasets/Wine
+df_wine = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data',header=None)
+df_wine.columns = ['Class label', 'Alcohol',
+                   'Malic acid', 'Ash',
+                   'Alcalinity of ash', 'Magnesium',
+                   'Total phenols', 'Flavanoids',
+                   'Nonflavanoid phenols',
+                   'Proanthocyanins',
+                   'Color intensity', 'Hue',
+                   'OD280/OD315 of diluted wines',
+                   'Proline']
+
+#print('Class labels',np.unique(df_wine['Class label']))
+#df_wine.head()
+
+#1.convenient way
+from sklearn.cross_validation import train_test_split
+X,y = df_wine.iloc[:,1:].values,df_wine.iloc[:,0].values
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.3,random_state = 0)
 
 
+#Bringing features onto same scale
+
+#1.normalization
+from sklearn.preprocessing import MinMaxScaler
+mms = MinMaxScaler()
+X_train_norm = mms.fit_transform(X_train)
+X_test_norm = mms.transform(X_test)
+
+#2.standardized
+from sklearn.preprocessing import StandardScaler
+stdsc = StandardScaler()
+X_train_std = stdsc.fit_transform(X_train)
+X_test_std = stdsc.transform(X_test)
+
+
+#Selecting meaningful features
+#1.Sparse solutions with L1 regularization
+from sklearn.linear_model import LogisticRegression
+lr = LogisticRegression(penalty = 'l1',C=0.1)
+lr.fit(X_train_std,y_train)
+print("Training accurarcy: ",lr.score(X_train_std,y_train));
+print("Test accurarcy: ",lr.score(X_test_std,y_test))
+lr.intercept_
+lr.coef_
+
+#2.Sequential feature selection algorithms
+from sklearn.neighbors import KNeighborsClassifier
+import matplotlib.pyplot as plt
+knn = KNeighborsClassifier(n_neighbors=2)
+import SBS
+sbs = SBS.SBS(knn,k_features=1)
+sbs.fit(X_train_std,y_train)
+
+k_feat = [len(list(k)) for k in sbs.subsets_]
+plt.plot(k_feat,sbs.scores_,marker='o')
+plt.ylim([0.7,1.1])
+plt.ylabel('Accuracy')
+plt.xlabel('Number of features')
+plt.grid()
+plt.show()
+
+#3 assessing feature importance with random forests
+from sklearn.ensemble import RandomForestClassifier
+feat_labels = df_wine.columns[1:]
+forest = RandomForestClassifier(n_estimators = 10000,random_state = 0,n_jobs = -1)
+forest.fit(X_train,y_train)
+importances = forest.feature_importances_
+indices = np.argsort(importances)[::-1]
+for f in range(X_train.shape[1]):
+    print("%2d) %-*s %f" % (f + 1, 30,feat_labels[f],importances[indices[f]]))
+
+plt.title('Feature Importances')
+plt.bar(range(X_train.shape[1]),
+importances[indices],
+color='lightblue',
+align='center')
+plt.xticks(range(X_train.shape[1]),
+feat_labels, rotation=90)
+plt.xlim([-1, X_train.shape[1]])
+plt.tight_layout()
+plt.show()
 
